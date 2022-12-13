bb023de02c34acecace3967691338a351e3543ae
mrwater <mrwater93@163.com>
2022-12-14 00:39:42 +0800

    搞不掂layaXML

--- TS_2D_DEMO_1122/src/scripts/ShopPanel.ts
+++ TS_2D_DEMO_1122/src/scripts/ShopPanel.ts
@@ -1,7 +1,7 @@
 import DataManage from "./DataManage";
 import { ui } from "../ui/layaMaxUI";
 
-export default class ShopPanel extends ui.shopPanelUI {
+export default class ShopPanel extends ui.StartSceneUI {
     constructor() {
         super();
 
@@ -15,14 +15,32 @@ export default class ShopPanel extends ui.shopPanelUI {
     
     Init(): void {
         this.txt_coin.text = (DataManage.Instance().getCoinCount()).toString();
+        this.shopSkin();
     }
-
 
     readConfigFile(): void {
-        let XmlRead = Laya.loader.load("res/CharactersSkin.csv", Laya.Handler.create(this, function(configFile){
-            console.log(configFile);
-        }))
+
+        let xmlDom = Laya.loader.load(
+            "res/CharactersSkinXml.xml", 
+            null, 
+            null,
+            Laya.Loader.XML,
+            )
+        console.log("xmlDom", xmlDom);
+        // let xmlDom = Laya.Utils.parseXMLFromString(_Res);
+
     }
 
+    /**
+     * 设置商品list的属性；
+     */
+    shopSkin(): void {
+        this.lst_skin.repeatX = 3;
+        this.lst_skin.spaceX = 450;
+        this.lst_skin.selectedIndex = -1;
 
+        this.lst_skin.elasticEnabled = false;
+        this.lst_skin.selectEnable = true;
+        this.lst_skin.visible = true;
+    }
 }
